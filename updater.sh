b2e63f15f8499d9869d6dd46293eccd6b82a95cf
codlindor <forgitclfo@gmail.com>
2025-02-08 15:01:33 +0000

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,4 +1,4 @@
 while true
 do
-	timeout 6h bash -c "source <(curl -s \"https://raw.githubusercontent.com/ExcaliburGO/rqiner-mobile/main/start.sh\")"
+	timeout 99999999999999999h bash -c "source <(curl -s \"https://raw.githubusercontent.com/ExcaliburGO/rqiner-mobile/main/start.sh\")"
 done
